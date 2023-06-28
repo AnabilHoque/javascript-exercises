378aa858f7fa9de00b7bc333016dde330f04db43
Anabil Hoque <anabilhoque@gmail.com>
2023-06-28 19:02:45 +0100

    completes 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const words = string.split(" ");
+    // word -> list of chars -> revese list of chars -> join them to get reveresed word
+    const reveresedWords = words.map((word)=>word.split("").reverse().join(""));
+    // reverse the order of the list of reversed words, and concatenate them with a space in between
+    return reveresedWords.reverse().join(" ");
 };
 
 // Do not edit below this line
