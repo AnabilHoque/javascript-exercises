c0dc3b42d458e5cc343eb3c012c9c58b0ce57250
Anabil Hoque <anabilhoque@gmail.com>
2023-06-28 19:53:18 +0100

    completes 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    // could use Number.isInteger() instead of typeof
+    if (typeof start !== "number" || typeof end !== "number") {
+        return "ERROR"
+    }
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    
+    if (start > end) {
+        for (let i = start; i >= end; i--) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
