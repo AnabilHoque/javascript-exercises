1107431c5091d59c1dd5461acd44dd886408668d
Anabil Hoque <anabilhoque@gmail.com>
2023-07-11 00:27:04 +0100

    completes 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    const removeSpacesAndPunctuation = s.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replaceAll(" ", "").toLowerCase();
+    let leftPtr = 0;
+    let rightPtr = removeSpacesAndPunctuation.length-1;
+    while (leftPtr <= rightPtr) {
+        if (removeSpacesAndPunctuation[leftPtr] !== removeSpacesAndPunctuation[rightPtr]) {
+            return false;
+        }
+        leftPtr++;
+        rightPtr--;
+    }
+    return true;
 };
 
 // Do not edit below this line
