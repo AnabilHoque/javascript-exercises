8da5c7da4c387643e6a8f657e3b67765764c4f12
Anabil Hoque <anabilhoque@gmail.com>
2023-07-11 21:42:43 +0100

    completes 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrOfObjects) {
+    return arrOfObjects.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentPersonAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        if (currentPersonAge > oldestAge) {
+            return currentPerson;
+        }
+        return oldest;
+    });
 };
+
+function getAge(birth, death) {
+    if (!death) {
+        return (new Date).getFullYear() - birth;
+    }
+    return death-birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
